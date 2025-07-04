3d992feea2180661a7e658786f16bc0faf07506e
Volikkk <illavolanuk02@gmail.com>
2025-07-04 11:12:13 +0300

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,18 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    public CommandLineRunner commandLineRunner() {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-            }
-        };
     }
 }
