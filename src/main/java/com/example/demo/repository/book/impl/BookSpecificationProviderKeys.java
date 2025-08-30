ba29a4392b913896167cb86c977f56c64d2c5b7a
Volikkk <illavolanuk02@gmail.com>
2025-08-30 14:55:18 +0300

    add new constant

---
+++ src/main/java/com/example/demo/repository/book/impl/BookSpecificationProviderKeys.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository.book.impl;
+
+public final class BookSpecificationProviderKeys {
+    public static final String AUTHORS = "authors";
+    public static final String TITLES = "titles";
+
+    private BookSpecificationProviderKeys() {
+    }
+}
+
