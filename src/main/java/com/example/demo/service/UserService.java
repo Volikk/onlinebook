f1dbb97da825b1f515d629b2835237c3d60ff4a9
Volikkk <illavolanuk02@gmail.com>
2025-09-08 23:32:31 +0300

    add interface and fix UserService

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,29 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.dto.UserRegistrationRequestDto;
 import com.example.demo.dto.UserResponseDto;
-import com.example.demo.entity.User;
 import com.example.demo.exception.RegistrationException;
-import com.example.demo.mapper.UserMapper;
-import com.example.demo.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-
-    public UserResponseDto register(UserRegistrationRequestDto requestDto)
-            throws RegistrationException {
-
-        if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new RegistrationException("Email already exists: " + requestDto.getEmail());
-        }
-
-        User user = userMapper.toUser(requestDto);
-        userRepository.save(user);
-        return userMapper.toUserResponse(user);
-    }
+public interface UserService {
+    UserResponseDto register(UserRegistrationRequestDto requestDto)
+            throws RegistrationException;
 }
