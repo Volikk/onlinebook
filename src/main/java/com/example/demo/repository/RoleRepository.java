b0125bc9362cf6fa934746ba17209be643523500
Volikkk <illavolanuk02@gmail.com>
2025-09-15 22:09:08 +0300

    change entityGraph

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -3,10 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.Role;
 import com.example.demo.entity.RoleName;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    @EntityGraph(attributePaths = {"roles"})
     Optional<Role> findByName(RoleName name);
 }
